3091dcb68955eb1c69d49682477209addecf45f5
User <amanatnorthorn@gmail.com>
2026-08-18 19:31:24 +0530

    fix(desktop): restore proper desktop header logo dimensions by removing conflicting global height auto

--- src/components/PlatformHeader.tsx
+++ src/components/PlatformHeader.tsx
@@ -227,7 +227,12 @@ export default function PlatformHeader() {
                 className="select-none cursor-pointer"
               >
                 {/* eslint-disable-next-line @next/next/no-img-element */}
-                <img src="/WIPA-Logo.png" alt="WIPA Logo" className="h-10 w-auto object-contain" />
+                <img 
+                  src="/WIPA-Logo.png" 
+                  alt="WIPA Logo" 
+                  className="h-10 max-h-10 w-auto max-w-[140px] object-contain shrink-0" 
+                  style={{ height: '40px', width: 'auto' }}
+                />
               </Link>
             </div>
             
